bad01dcee31ad7c644de5a17dc80d2dba94f7a16
Senthamizh-tech <in.ent.common.123@gmail.com>
2024-07-11 13:57:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-var Application = require("./lib/app");
-var Server      = require("./lib/server");
+// var Application = require("./lib/app");
+// var Server      = require("./lib/server");
 var sdk         = require("./lib/sdk");
-var config      = require("./config");
+// var config      = require("./config");
 
-var app    = new Application(null, config);
-var server = new Server(config, app);
+// var app    = new Application(null, config);
+// var server = new Server(config, app);
+
+const express = require('express');
+const APIKeyMiddleware = require("./lib/app/middlewares/APIKeyMiddleware");
+const app = express()
+const port = 8003;
 
 sdk.checkNodeVersion();
 
-server.start();
+app.use(bodyparser());
+
+var apikeyMW = APIKeyMiddleware({});
+app.use('../sdk/', apikeyMW);
+
+app.listen(port , function(){
+    var host = 'localhost';
+    console.info('app listening at http://%s:%s', host, port);
+});
+
+// server.start();
 
 // sdk.registerBot(require('./FindAFlight.js'));
 sdk.registerBot(require('./SimpleConversationalBot.js'));
