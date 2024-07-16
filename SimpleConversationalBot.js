b6f6ae61611b6a67ca51d74bf7f9f1c6cda9a34a
Senthamizh-tech <in.ent.common.123@gmail.com>
2024-07-16 16:33:37 +0530

    Update SimpleConversationalBot.js

--- SimpleConversationalBot.js
+++ SimpleConversationalBot.js
@@ -49,7 +49,8 @@ module.exports = {
             ...data,
            "msg":"Spell-corrected message sent by the assistant to the user",
         }
-        console.log("Modified data ===> ", mod_data.context.session.BotUserSession.lastMessage);
+        console.log("Modified data ===> ", mod_data.context.session.BotUserSession);
+        console.log("Modified data ===> ", mod_data.context.session);
         return sdk.sendUserMessage(mod_data, callback);
         // sdk.getSavedData(requestId)
         //     .then(() => {
